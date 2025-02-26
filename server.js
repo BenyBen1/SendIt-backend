c83d86940790f29286bb4990434754311d9eb736
Collins Ndung'u <collinsndungu953@gmail.com>
2025-02-26 11:34:44 +0300

    update server.js , orders.js and ordersRoute.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose=require('mongoose');
 
 const app = express();
 const port = 5000;
+
+
+mongoose.connect("mongodb://localhost:27017/campsiteDB")
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
 
 // Middleware
 app.use(cors());
