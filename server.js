2ad3efb87b141ff245d64fbd186178c8aea526e0
Ben Wachira <gishuks14@gmail.com>
2025-03-01 15:43:45 +0300

    add shipment details model and route; include CORS middleware

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
+
+mongoose.connect('mongodb://localhost:27017/userDb')
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Import the orders router
+const shipmentDetailsRouter = require('./routes/shipmentDetailsRoute');
 const ordersRouter = require('./routes/ordersRoute');
+
+// API endpoints
 app.use('/orders', ordersRouter);
+app.use('/shipment-details', shipmentDetailsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`✅ Server running on http://localhost:${port}`);
 });
